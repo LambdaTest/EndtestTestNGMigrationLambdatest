c55ff145c5c8dc85e3dfbeda667b9767c56ce61d
Ankit Agarwal <admin@Saurabhs-MacBook-Pro.local>
2021-10-25 12:50:19 +0530

    endtest apis added

--- src/main/java/MongoServices/service/EndTestService.java
+++ src/main/java/MongoServices/service/EndTestService.java
@@ -1,12 +1,20 @@
 package MongoServices.service;
 
+import MongoServices.DTO.request.EndTestProjectSuiteLinkingDTO;
+import MongoServices.DTO.response.ResponseDto;
+import MongoServices.common.Constants;
 import MongoServices.mongo.entity.EndTestDocumentDAO;
+import MongoServices.mongo.entity.EndTestProjectSuiteLinkingDAO;
 import MongoServices.service.impl.EndTestServiceImpl;
+import MongoServices.transformer.TransformingDtoToDao;
+import com.mongodb.client.result.UpdateResult;
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -27,6 +35,33 @@ public class EndTestService implements EndTestServiceImpl {
     Query query = new Query();
     query.addCriteria(Criteria.where(field).is(value));
     return mongoTemplate.find(query,EndTestDocumentDAO.class);
+  }
+
+  @Override
+  public ResponseDto saveTheLinkingData(EndTestProjectSuiteLinkingDTO endTestProjectSuiteLinkingDTO) {
+    ResponseDto responseDto = new ResponseDto();
+    Query query = new Query();
+    query.addCriteria(Criteria.where("suiteId").is(endTestProjectSuiteLinkingDTO.getSuiteId()));
+      Document document = new Document();
+      mongoTemplate.getConverter().write(endTestProjectSuiteLinkingDTO,document);
+     UpdateResult updateResult = mongoTemplate.upsert(query, Update.fromDocument(document)
+       , EndTestProjectSuiteLinkingDAO.class, Constants.endTestSuite);
+
+     if ((updateResult.getMatchedCount() == 0 && updateResult.getUpsertedId()!=null) ||
+       (updateResult.getMatchedCount() > 0 && updateResult.getUpsertedId() == null)) {
+       responseDto.setSuccessCode(Constants.statusCode);
+       responseDto.setSuccessMessage(Constants.successMessage);
+     }else {
+       responseDto.setErrorCode(Constants.errorCode);
+       responseDto.setErrorMessage(Constants.errorMessage);
+     }
+    return responseDto;
+  }
+
+  public EndTestProjectSuiteLinkingDTO getDataFromLinkingDB(String suiteId){
+    TransformingDtoToDao transformingDtoToDao = new TransformingDtoToDao();
+    Query query = new Query().addCriteria(Criteria.where("suiteId").is(suiteId));
+    return transformingDtoToDao.getTransformedData(mongoTemplate.findOne(query,EndTestProjectSuiteLinkingDAO.class));
   }
 
 }
--- src/main/java/MongoServices/service/impl/EndTestServiceImpl.java
+++ src/main/java/MongoServices/service/impl/EndTestServiceImpl.java
@@ -1,5 +1,7 @@
 package MongoServices.service.impl;
 
+import MongoServices.DTO.request.EndTestProjectSuiteLinkingDTO;
+import MongoServices.DTO.response.ResponseDto;
 import MongoServices.mongo.entity.EndTestDocumentDAO;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +11,8 @@ public interface EndTestServiceImpl {
         List<String> getDistinctValuesBasedOnField(String field);
 
         List<EndTestDocumentDAO> getResultBasedOnFieldValue(String field, String value);
+
+        ResponseDto saveTheLinkingData(EndTestProjectSuiteLinkingDTO endTestProjectSuiteLinkingDTO);
+
+        EndTestProjectSuiteLinkingDTO getDataFromLinkingDB(String suiteId);
 }
